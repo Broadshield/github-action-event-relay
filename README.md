1241827ca18d1f9f75ffbe81882603f1d91a975f
Jamie Nelson <jamie@wearsafe.com>
2020-06-10 13:08:43 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# github-action-event-listener
+# github-action-event-relay
+
---
+++ index.js
@@ -0,0 +1,40 @@
+
+const core = require('@actions/core')
+const github = require('@actions/github')
+const YAML = require('yaml')
+
+try {
+    (async () => {
+        const token = core.getInput('github_token')
+        const destinationRepo = core.getInput('destination_repository')
+        const eventType = core.getInput('event_type')
+        const event = core.getInput('event') !== null ? JSON.loads(core.getInput('event')) : null
+        const jsonPayload = core.getInput('extra_payload_json') !== null ? JSON.loads(core.getInput('extra_payload_json')) : null
+        const yamlPayload = core.getInput('extra_payload_yaml') !== null ? YAML.loads(core.getInput('extra_payload_yaml')) : null
+
+        const extraPayload = {
+            ...jsonPayload,
+            ...yamlPayload,
+        }
+
+        const [owner,repo] = destinationRepo.split('/')
+
+        const { context, eventName } = github
+
+        const clientPayload = {
+            origin_context: context,
+            origin_eventName: eventName,
+            event: event || context,
+            extraPayload: extraPayload,
+        }
+
+        const octokit = new github.GitHub(token)
+
+        await octokit.repos.createDispatchEvent({
+            owner: owner,
+            repo: repo,
+            client_payload: clientPayload,
+            event_type: eventType,
+        })
+    })()
+} catch (error) { core.setFailed(error.message) }
